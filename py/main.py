260cd34a1018842e242e7772f1b2a3131888980d
Aanu Olakunle <olakunleaanu@gmail.com>
2025-08-22 20:58:14 +0200

    add latest YOLO tracking

--- py/main.py
+++ py/main.py
@@ -6,12 +6,32 @@ from real_time_object_tracker import RealTimeObjectTracker
 from ultralytics import YOLO
 
 
-def main():
+def YOLO11(images_path):
     model = YOLO("yolo11s.pt")
 
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.join(script_dir, "..", "data")
-    images_path = sorted(glob.glob(os.path.join(data_dir, "*.png")))
+    for image_file in images_path:
+        result = model.track(
+            image_file, conf=0.5, persist=True, tracker="bytetrack.yaml"
+        )[0]
+
+        if result.boxes and result.boxes.is_track:
+
+            # Visualize the result on the frame
+            annotated_frame = result.plot(conf=False)
+
+        # Display the annotated frame
+        cv.imshow("YOLO11 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    # Clean up OpenCV windows
+    cv.destroyAllWindows()
+
+
+def simpleSortAlgorithm(images_path):
+    model = YOLO("yolo11s.pt")
 
     height, width = 360, 1280
     tracker = RealTimeObjectTracker(model, width, height)
@@ -26,9 +46,8 @@ def main():
         new_image = cv.cvtColor(output_image, cv.COLOR_RGB2BGR)
         cv.imshow("Object Tracking", new_image)
 
-        # Wait for 1/30 second (66.67 ms) to achieve 30 FPS
-        key = cv.waitKey(33)
-        if key == ord("q") or key == 27:  # 'q' key or ESC to quit
+        # Break the loop if 'q' is pressed
+        if cv.waitKey(1) & 0xFF == ord("q"):
             break
 
     # Clean up OpenCV windows
@@ -36,4 +55,10 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(script_dir, "..", "data")
+    images_path = sorted(glob.glob(os.path.join(data_dir, "*.png")))
+
+    YOLO11(images_path)
+
+    # simpleSortAlgorithm(images_path)
